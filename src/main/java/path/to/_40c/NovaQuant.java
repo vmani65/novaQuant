1d5dee161c08b62babd61d52b60042bcb00c1563
Vinoth <vmani65@gmail.com>
2026-03-14 22:46:55 -0400

    Test

--- src/main/java/path/to/_40c/NovaQuant.java
+++ src/main/java/path/to/_40c/NovaQuant.java
@@ -2,7 +2,7 @@ package path.to._40c;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class NovaQuant {
   public static void main(String[] args) {
