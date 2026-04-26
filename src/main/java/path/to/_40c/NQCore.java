d2653731760ed34ec6da0fd47a02667690bd52ce
Vinoth <vmani65@gmail.com>
2026-04-26 14:33:50 +0530

    Rename Project.

--- src/main/java/path/to/_40c/NQCore.java
+++ src/main/java/path/to/_40c/NQCore.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NovaQuant {
+public class NQCore {
   public static void main(String[] args) {
-    SpringApplication.run(NovaQuant.class, args);
+    SpringApplication.run(NQCore.class, args);
   }
 }
